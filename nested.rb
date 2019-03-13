f55abbfe9e77c35fdebc0179341492513b8a0b98
Jeff M <jkdmarks@gmail.com>
2019-03-13 00:02:34 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -58,7 +58,7 @@ def dennis_ritchies_language
         }
      }
      
-  programmer_hash[:dennis_ritchie][:languages].each { |x| puts x }
+   programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
